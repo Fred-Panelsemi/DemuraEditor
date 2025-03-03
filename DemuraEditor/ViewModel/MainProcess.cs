c62136ec35966b9150a471126f6ded1798be6833
峰瑜 張 <fred.chang@panelsemi.com>
2025-03-03 13:33:45 +0800

    加入專案檔案。

---
+++ DemuraEditor/ViewModel/MainProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemuraEditor.ViewModel
+{
+    internal class MainProcess
+    {
+        private Version mVersion;
+        public string DemuraEditor_Version { get; }
+        public MainProcess()
+        {
+            mVersion = new Version();
+            DemuraEditor_Version = $"v{mVersion.GetVersion()}";
+            
+        }
+    }
+}
---
+++ DemuraEditor/ViewModel/Version.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemuraEditor.ViewModel
+{
+    internal class Version
+    {
+        public string GetVersion()
+        {
+            /* 版本紀錄 */
+            // 1.0.0.0 【初版】
+            return "1.0.0.0";
+        }
+    }
+}
